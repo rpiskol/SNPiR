ab44e83dd04bff274575608c20c9fce7cdf6ce1f
Robert Piskol <piskol@stanford.edu>
2014-01-22 10:38:23 -0800

    fixed package descriptions

--- config.pm
+++ config.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-package SNPiR::config;
+package snpir::config;
 
 use lib qw(../);
 
@@ -14,13 +14,9 @@ use base 'Exporter';
 #
 # configuration file
 
-#our $BLATEXE = '/path/to/blat/executable'; #this is the path to the blat executable
-#our $SAMTOOLSEXE = '/path/to/samtools/executable'; #this is the path to the samtools executable
-#our $FASTAFROMBED = '/path/to/fastaFromBed'; #this is the path to the fastaFromBed executable in the bedtools package 
-
-our $BLATEXE = '/srv/gs1/projects/li/shared-data/software/kent/src/blat/blat'; #this is the path to the blat executable
-our $SAMTOOLSEXE = '/srv/gs1/projects/li/shared-data/software/samtools-0.1.16/samtools'; #this is the path to the samtools executable
-our $FASTAFROMBED = '/srv/gs1/projects/li/shared-data/software/bedtools-2.17.0/bin/fastaFromBed'; #this is the path to the fastaFromBed executable in the bedtools package 
+our $BLATEXE = '/path/to/blat/executable'; #this is the path to the blat executable
+our $SAMTOOLSEXE = '/path/to/samtools/executable'; #this is the path to the samtools executable
+our $FASTAFROMBED = '/path/to/fastaFromBed'; #this is the path to the fastaFromBed executable in the bedtools package 
 
 our @EXPORT = (
         '$BLATEXE',
